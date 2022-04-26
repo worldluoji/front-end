7cf49f08183a5aa832cfb669383f55b2ca461938
worldluoji <qq1132416@outlook.com>
2022-04-26 14:27:20 +0800

    @require@luoji@20220426

--- js/basics/require/demo1/a.js
+++ js/basics/require/demo1/a.js
@@ -1,7 +1,11 @@
-var b = require('./b.js')
-var c = require('./c.js')
+// require有缓存机制，两次导入同一个.js文件，第二次直接使用上一次的缓存
+// 特殊情况下第二次如果需要重新导入，需要先 delete require.cache[require.resolve(path)]
+const b = require('./b.js')
+const c = require('./c.js')
+const c2 = require('./c.js')
 
 b.printInfo()
 c()
 c({})
 c({name: 'luoji'})
+console.log(c === c2)
