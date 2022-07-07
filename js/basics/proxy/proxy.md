fff82c70ed6f3cb017c811d40a4669c6b47bd427
worldluoji <qq1132416@outlook.com>
2022-07-07 18:42:39 +0800

    feat: js proxy demo

---
+++ js/basics/proxy/proxy.md
@@ -0,0 +1,14 @@
+# Proxy
+Proxy 对象用于创建一个对象的代理，从而实现基本操作的拦截和自定义（如属性查找、赋值、枚举、函数调用等）。
+
+
+## 语法
+```
+const p = new Proxy(target, handler)
+```
+参数说明：
+- target: 要使用 Proxy 包装的目标对象（可以是任何类型的对象，包括原生数组，函数，甚至另一个代理）。
+- handler: 一个通常以函数作为属性的对象，各属性中的函数分别定义了在执行各种操作时代理 p 的行为。
+
+## 参考
+- https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Proxy
---
+++ js/basics/proxy/proxy_demo3.js
@@ -0,0 +1,31 @@
+let validator = {
+    set: function(obj, prop, value) {
+      if (prop === 'age') {
+        if (!Number.isInteger(value)) {
+          throw new TypeError('The age is not an integer');
+        }
+        if (value > 200) {
+          throw new RangeError('The age seems invalid');
+        }
+      }
+  
+      // The default behavior to store the value
+      obj[prop] = value;
+  
+      // 表示成功
+      return true;
+    }
+};
+  
+let person = new Proxy({}, validator);
+
+person.age = 100;
+
+console.log(person.age);
+// 100
+
+person.age = 'young';
+// 抛出异常: Uncaught TypeError: The age is not an integer
+
+person.age = 300;
+// 抛出异常: Uncaught RangeError: The age seems invalid
