257a73ec07a2711265faf779c363289c66c2e9a8
worldluoji <qq1132416@outlook.com>
2022-07-11 15:27:45 +0800

    feat: Vue3使用JSX

---
+++ vue/vue3-demos/src/components/render/JSXComponent.vue
@@ -0,0 +1,15 @@
+<!-- https://staging-cn.vuejs.org/guide/extras/render-function.html#jsx-tsx -->
+<!-- 使用typescript，这里要用tsx -->
+<script lang="tsx">
+export default {
+  props: {
+    username: { type: String },
+  },
+  setup(props) {
+    // 使用JSX语法
+    const vNode = <div>hello {props.username}</div>;
+    // 返回渲染函数
+    return () => vNode;
+  },
+};
+</script>
