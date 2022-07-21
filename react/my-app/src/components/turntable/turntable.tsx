b4707e554264251e51e7d093890f67015a556a9d
worldluoji <qq1132416@outlook.com>
2022-07-21 14:10:11 +0800

    fix: 增加抽奖条目显示

--- react/my-app/src/components/turntable/turntable.tsx
+++ react/my-app/src/components/turntable/turntable.tsx
@@ -1,11 +1,63 @@
 import React from "react";
 import './turntable.scss';
 
+const prizeItems = [
+    {
+      name: "专属优惠",
+      img: "https://cdn-icons-png.flaticon.com/512/1405/1405225.png"
+    },
+    {
+      name: "变形金刚",
+      img: "https://cdn-icons-png.flaticon.com/512/7926/7926936.png"
+    },
+    {
+      name: "铁锅三件套",
+      img: "https://cdn-icons-png.flaticon.com/512/3063/3063504.png"
+    },
+    {
+      name: "格子吐司机",
+      img: "https://cdn-icons-png.flaticon.com/512/4353/4353006.png"
+    },
+    {
+      name: "折扣券",
+      img: "https://cdn-icons-png.flaticon.com/512/612/612885.png"
+    },
+    {
+      name: "红包袋",
+      img: "https://cdn-icons-png.flaticon.com/512/677/677721.png"
+    },
+    {
+      name: "迪士尼随手瓶",
+      img: "https://cdn-icons-png.flaticon.com/512/4982/4982355.png"
+    },
+    {
+      name: "黑山牛排",
+      img: "https://cdn-icons-png.flaticon.com/512/5854/5854248.png"
+    },
+];
+
+interface TypePrize {
+    prizeName: string,
+    url: string
+}
+
+const Prize = ( {prizeName, url}:TypePrize) => (
+    <li>
+        <p>{ prizeName }</p>
+        <img src={ url } />
+    </li>
+)
+
+const PrizeList = () => {
+    const list = prizeItems.map(item => <Prize prizeName={item.name} url={item.img} />);
+    return <ul>{ list }</ul>
+}
+
 const Turntable = () => (
     <div className="container">
     <section className="turntable">
         <div className="list">
-            <ul></ul>
+          <PrizeList />
         </div>
         <button className="turntable_btn" type="button">GO</button>
         <svg viewBox="-50 -50 100 100">
