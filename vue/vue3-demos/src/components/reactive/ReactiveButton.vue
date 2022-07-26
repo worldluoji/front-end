6f155f7f0488be5b2b77b43b9d9965f32c1a9944
worldluoji <qq1132416@outlook.com>
2022-07-26 18:40:44 +0800

    fix: 响应式原理补充

--- vue/vue3-demos/src/components/reactive/ReactiveButton.vue
+++ vue/vue3-demos/src/components/reactive/ReactiveButton.vue
@@ -1,12 +1,12 @@
 <!-- https://staging-cn.vuejs.org/guide/essentials/reactivity-fundamentals.html#script-setup -->
 <script setup>
-import { reactive, nextTick } from "vue";
+import { effect, reactive, ref, nextTick } from "vue";
 
 const state = reactive({ count: 0 });
 
 function increment() {
   state.count++;
-  console.log("count++");
+  counter.num1++;
   // 你得注意 DOM 的更新并不是同步的。相反，Vue 将缓冲它们直到更新周期的 “下个时机” 以确保无论你进行了多少次声明更改，每个组件都只需要更新一次。
   // 若要等待一个状态改变后的 DOM 更新完成，你可以使用 nextTick() 这个全局 API
   nextTick(() => {
@@ -14,9 +14,18 @@ function increment() {
     console.log("Dom更新完毕");
   });
 }
+
+const counter = reactive({ num1: 1, num2: 2 });
+let dummy = ref(0);
+// 当counter.num1或者counter.num2改变，effect中的方法就会执行，从而更新dummy
+effect(() => {
+  dummy.value = counter.num1 + counter.num2;
+  console.log(dummy.value); // 每次counter.num1修改都会打印日志
+});
 </script>
 
 <template>
+  <p>{{ dummy }}</p>
   <button @click="increment">
     {{ state.count }}
   </button>
--- vue/vue3-demos/src/components/reactive/reactive.md
+++ vue/vue3-demos/src/components/reactive/reactive.md
@@ -11,3 +11,15 @@ let state = reactive({ count: 0 })
 // 上面的引用 ({ count: 0 }) 将不再被追踪（响应性连接已丢失！）
 state = reactive({ count: 1 })
 ```
+
+## demo
+一个最简单的响应式模型，我们可以通过 reactive 或者 ref 函数，把数据包裹成响应式对象，并且通过 effect 函数注册回调函数，然后在数据修改之后，响应式地通知 effect 去执行回调函数即可。
+具体见 ReactiveButton.vue 、DeepReactive.vue 
+
+### 响应式原理
+
+<img src="响应式原理示意图.png" />
+
+在 effect 中获取 counter.num1 和 counter.num2 的时候，就会触发 counter 的 get 拦截函数；get 函数，会把当前的 effect 函数注册到一个全局的依赖地图中去。这样 counter.num1 在修改的时候，就会触发 set 拦截函数，去依赖地图中找到注册的 effect 函数，然后执行。
+
+reactive 是通过 ES6 中的 Proxy 特性实现的属性拦截.
