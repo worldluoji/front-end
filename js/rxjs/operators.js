b9f22ad25851ad7a820a58b0c60bf24fe4336f92
worldluoji <qq1132416@outlook.com>
2022-08-16 15:34:47 +0800

    feat: add rxjs catcherror and redux demo

--- js/rxjs/operators.js
+++ js/rxjs/operators.js
@@ -1,4 +1,4 @@
-import { of, map, first, interval, pipe, filter} from 'rxjs';
+import { of, map, first, interval, pipe, filter, catchError, reduce, take} from 'rxjs';
 
 // 创建操作
 of(1, 2, 3)
@@ -27,3 +27,46 @@ function discardOddDoubleEven() {
 of(1,2,3,4,5,6)
     .pipe(discardOddDoubleEven())
     .subscribe((v) => console.log(`value: ${v}`))
+
+
+// catchError
+of(1, 2, 3, 4, 5)
+  .pipe(
+    map(n => {
+      if (n === 4) {
+        throw 'four!';
+      }
+      return n;
+    }),
+    catchError(err => of('I', 'II', 'III', 'IV', 'V'))
+  )
+  .subscribe(x => console.log(x));
+  // 1, 2, 3, I, II, III, IV, V
+
+
+of(1, 2, 3, 4, 5)
+  .pipe(
+    map(n => {
+      if (n === 4) {
+        throw 'four!';
+      }
+      return n;
+    }),
+    catchError((err, caught) => caught),
+    take(10)
+  )
+  .subscribe(x => console.log(x));
+  // 1, 2, 3, 1, 2, 3, ...
+
+// reduce
+const res = {
+  errors: [],
+  data: {}
+}
+const obs = of({id:1, name:'apple'}, {id:2, name:'orange'})
+// 第一次，没有前一个值时，acc的值就是res, item则是obs中的一个对象
+obs.pipe(reduce((acc, item, i) => { 
+  acc.data[item.id] = item
+  return acc
+}, res))
+.subscribe(x => console.log(x))
