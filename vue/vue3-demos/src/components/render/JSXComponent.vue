168630d1865e2ce622ba399fc22b296aa52c4fcb
worldluoji <qq1132416@outlook.com>
2022-08-17 15:56:52 +0800

    doc: 对h函数和JSX补充说明

--- vue/vue3-demos/src/components/render/JSXComponent.vue
+++ vue/vue3-demos/src/components/render/JSXComponent.vue
@@ -13,3 +13,6 @@ export default {
   },
 };
 </script>
+<!-- JSX 可以支持更动态的需求。而 template 则因为语法限制原因，不能够像 JSX 那样可以支持更动态的需求。
+这是 JSX 相比于 template 的一个优势
+我们实现业务需求的时候，也是优先使用 template，动态性要求较高的组件使用 JSX 实现 -->
