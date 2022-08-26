1b5a6afbf4044e83e2adf3514a59faac88ecb504
worldluoji <qq1132416@outlook.com>
2022-08-26 16:56:21 +0800

    feat: add element-factory 项目，带注释

---
+++ vue/vue低代码原理/element-factory/src/generator.mjs
@@ -0,0 +1,90 @@
+import element from './nodes/element.mjs'
+import text from './nodes/text.mjs'
+import Code from './code.mjs'
+import comment from './nodes/comment.mjs'
+
+const typeMap = {
+    1: element,
+    2: text,
+    3: comment,
+}
+
+// 遍历AST抽象树，第一次进入时，parent就是ast
+function walk(parent, fn) {
+    if (parent.children && parent.children.length > 0) {
+        parent.children.forEach(child => {
+            fn(child)
+        })
+    }
+}
+
+function toHyphenCase(str) {
+    const char = str.charAt(0)
+    const newStr = char.toLowerCase() + str.slice(1)
+    return newStr.replace(/([A-Z])/g, (value, replaceValue) => {
+        return '-' + replaceValue.toLowerCase()
+    })
+}
+
+export default class Generator {
+    constructor(ast, options) {
+        this.ast = ast
+        this.options = options
+        this.depencencies = []
+        this.props = []
+        this.visit(this.ast)
+        console.log('depencencies === ', this.depencencies)
+    }
+
+    addDpe(name) {
+        if (this.depencencies.indexOf(name) < 0) {
+            this.depencencies.push(name)
+        }
+    }
+
+    expression(exp) {
+        const { content } = exp
+        if (this.props.indexOf(content) < 0) {
+            this.props.push(exp.content)
+            console.log(this.props)
+        }
+        // TODO: this should parse the expression
+    }
+
+    visit(parent) {
+        const generator = this
+        walk(parent, (child) => {
+            const typeHandler = typeMap[child.type]
+            if (typeHandler) {
+                let enter, leave
+                if (typeof typeHandler === 'object') {
+                    enter = typeHandler.enter
+                    leave = typeHandler.leave
+                } else {
+                    enter = typeHandler
+                }
+                enter && enter(generator, child)
+                this.visit(child)
+                leave && leave(generator, child)
+            } else {
+                throw new Error(`Node type = ${child.type} is not defined`)
+            }
+        })
+    }
+
+    buildScript() {
+        const code = new Code
+        const resolve = this.options.resolve || ((elementName) => { 
+            if (elementName.indexOf('Tpl_') > -1) {
+                return `./${elementName.slice(4)}.tpl`
+            } else {
+                return `./elements/${toHyphenCase(elementName)}.vue`
+            }
+        })
+        this.depencencies.forEach(dep => {
+            code.addLine(`import ${dep} from '${resolve(dep)}'`)
+        })
+
+        return code.toString()
+    }
+}
---
+++ vue/vue低代码原理/element-factory/src/index.mjs
@@ -0,0 +1,31 @@
+import { parse, compileScript, compileTemplate } from '@vue/compiler-sfc'
+import Generator from './generator.mjs'
+
+export default (code, options = {}) => {
+    const parsed = parse(`<template>${code}</template>`)
+    const ast = parsed.descriptor.template.ast
+    const generator = new Generator(ast, options)
+    const newParsed = parse(`
+<script setup>
+${generator.buildScript()}
+</script>  
+<script>
+export default {
+    data: (el) => {
+        console.log('data === ', el.$attrs)
+        return el.$attrs
+    }
+}
+</script>
+<template>
+${code}
+</template>
+`)
+    const ret = compileScript(newParsed.descriptor, { 
+        id: 'abc',
+        inlineTemplate: true
+    })
+
+    console.log(ret.content)
+    return ret.content
+}
