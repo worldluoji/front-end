549b3b9151bc219b886f5bc72464e684df97d0b0
worldluoji <qq1132416@outlook.com>
2022-11-07 10:12:43 +0800

    feat: add service worker demo

---
+++ pwa/cache/js/script.js
@@ -0,0 +1,3 @@
+function helloCache() {
+    console.log('hello cache')
+}
---
+++ pwa/cache/service-worker1.js
@@ -0,0 +1,29 @@
+// 缓存名称
+const cacheName = 'helloCache'
+
+// 监听service worker install事件
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                '../ServiceWorker的拦截作用.webp',
+                './js/script.js'
+            ]))
+        // 将以上图片和js加入到缓存中
+    )
+})
+
+// 监听fetch事件
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        // match检查请求的URL是否命中缓存，命中则直接返回缓存内容，否则仍然走fetch进行请求
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('using cache', response)
+                    return response
+                }
+                return fetch(event.request)
+            })
+    )
+})
