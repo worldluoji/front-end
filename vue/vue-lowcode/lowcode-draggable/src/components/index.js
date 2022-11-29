a91ff7673129545cbcdfade06a4a83d8df4c228f
worldluoji <qq1132416@outlook.com>
2022-11-29 19:01:21 +0800

    fix: remove Blank in index

--- vue/vue-lowcode/lowcode-draggable/src/components/index.js
+++ vue/vue-lowcode/lowcode-draggable/src/components/index.js
@@ -1,11 +1,9 @@
-import Blank from './Blank.vue'
 import Image from './Image.vue'
 import NavBar from './NavBar.vue'
 import Offer from './Offer.vue'
 import List from './List.vue'
 
 export default {
-    Blank,
     Image,
     NavBar,
     Offer,
