dd272c5b7d6f1d22f5e98336759ab2915ddef15b
worldluoji <qq1132416@outlook.com>
2022-11-29 13:40:05 +0800

    doc: 修改注释

--- vue/vue-lowcode/lowcode-draggable/src/router/router.js
+++ vue/vue-lowcode/lowcode-draggable/src/router/router.js
@@ -13,7 +13,7 @@ const routes = [
 // 你可以在这里输入更多的配置，但我们在这里暂时保持简单
 console.log(process.env)
 const router = createRouter({
-    // 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    // 内部提供了 history 模式的实现。
     history: createWebHistory(process.env.BASE_URL),
     routes, // `routes: routes` 的缩写
 });
