222855b356c0ec67714412ee45f15be5f0c2b72f
worldluoji <qq1132416@outlook.com>
2022-12-02 16:05:50 +0800

    fix: playwright 配置

--- vite/island-ssg/playwright.config.ts
+++ vite/island-ssg/playwright.config.ts
@@ -2,10 +2,13 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
+  // Each test is given 30 seconds
   timeout: 30000,
+  // To launch a server during the tests, use the webServer option
   webServer: {
-    url: 'http://localhost:5175',
-    command: 'npm run prepare:e2e'
+    command: 'npm run prepare:e2e',
+    url: 'http://localhost:5175', // If url is specified in the config, test runner will wait for that url to return a 2xx, 3xx, 400, 401, 402, or 403 response before running the tests.
+    timeout: 120 * 1000
   },
   use: {
     headless: true
@@ -13,3 +16,5 @@ const config: PlaywrightTestConfig = {
 };
 
 export default config;
+
+// playwright配置参考：https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
