984758e0a3e35398a65d66f3be750688702c7a5c
worldluoji <qq1132416@outlook.com>
2023-01-10 20:09:30 +0800

    feat: node静态资源服务器简单实现

---
+++ js/node/static-server/index.js
@@ -0,0 +1,44 @@
+import http from "http";
+import Path from "path";
+import fs from "fs";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = Path.dirname(__filename);
+
+var server = http.createServer(function (req, res){
+    const fileName = Path.resolve(__dirname, 'dist', "." + req.url);
+    const extName = Path.extname(fileName).substr(1);
+
+    if (fs.existsSync(fileName)) { //判断本地文件是否存在
+        var mineTypeMap={
+            html:'text/html;charset=utf-8',
+            htm:'text/html;charset=utf-8',
+            xml:"text/xml;charset=utf-8",
+            png:"image/png",
+            jpg:"image/jpeg",
+            jpeg:"image/jpeg",
+            gif:"image/gif",
+            css:"text/css;charset=utf-8",
+            txt:"text/plain;charset=utf-8",
+            mp3:"audio/mpeg",
+            mp4:"video/mp4",
+            ico:"image/x-icon",
+            tif:"image/tiff",
+            svg:"image/svg+xml",
+            zip:"application/zip",
+            ttf:"font/ttf",
+            woff:"font/woff",
+            woff2:"font/woff2",
+            js:"application/x-javascript"
+        }
+        if (mineTypeMap[extName]) {
+            res.setHeader('Content-Type', mineTypeMap[extName]);
+        }
+        res.setHeader("Access-Control-Allow-Origin", "*"); // 设置可访问的源
+        res.setHeader("Access-Control-Allow-Headers", "*"); 
+        var stream = fs.createReadStream(fileName);
+        stream.pipe(res);
+    }
+})
+server.listen(80);
