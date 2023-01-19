06b1b0f57196037192548a7f8223b92c2ba9d2d5
worldluoji <qq1132416@outlook.com>
2023-01-19 15:21:10 +0800

    refactor: delete spm

--- typescript/spm/src/utils.ts
+++
@@ -1,8 +0,0 @@
-
-export function GetQueryString(name: string): string|null {
-    const url_string = window.location.href
-    const url = new URL(url_string);
-    return url.searchParams.get(name)
-}
-
-
