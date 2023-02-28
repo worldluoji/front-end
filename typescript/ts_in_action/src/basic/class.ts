66380a87b5d111026b5b767f8fb66289c62cd9f2
worldluoji <qq1132416@outlook.com>
2023-02-28 19:42:29 +0800

    refactor: typescript class整理

--- typescript/ts_in_action/src/basic/class.ts
+++ typescript/ts_in_action/src/basic/class.ts
@@ -71,3 +71,26 @@ class MyFlow extends Workflow {
     }
 }
 new MyFlow().next().step1().next().step2()
+
+
+// Student有firstName和lastName，他就是一个Person, 这就允许我们在实现接口时候只要保证包含了接口要求的结构就可以，而不必明确地使用 implements语句
+class Student {
+    fullName: string;
+    // 注意的是，在构造函数的参数上使用public等同于创建了同名的成员变量
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+console.log(greeter(user));
