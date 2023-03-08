7e2b56c0ddf5e6e3b6d9c71de0a18b322db552bd
worldluoji <qq1132416@outlook.com>
2023-03-08 15:20:15 +0800

    refactor: 补充setTimout

--- js/node/nextTick.js
+++ js/node/nextTick.js
@@ -11,6 +11,13 @@ setImmediate(() => {
   console.log("Running before the timeout => number 3");
 });
 
+setTimeout(() => {
+    console.log("The timeout running last => number 4.2");
+}, 10);
+setTimeout(() => {
+    console.log("The timeout running last => number 4.3");
+}, 0);
+
 process.nextTick(() => {
   console.log("Running at next tick => number 2");
 });
