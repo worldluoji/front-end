2eeef8a17f4abd13798ca64d11401dfcec186973
worldluoji <qq1132416@outlook.com>
2023-08-08 09:49:58 +0800

    feat: reflect receiver demo

--- js/basics/proxy/reflect-receiver.js
+++ js/basics/proxy/reflect-receiver.js
@@ -2,25 +2,34 @@ const obj = {
     _name: "test",
     get name() {
         // 这里的this默认指向obj，但是通过Reflect.get修改了this指向
-        return this._name
+        console.log('get', this === obj);
+        return this._name;
     },
     set name(newValue) {
-        this._name = newValue
+        console.log('set', this === obj);
+        this._name = newValue;
     }
 }
 
 const objProxy = new Proxy(obj, {
     get: function(target, key, receiver) {
         // receiver是创建出来的代理对象
-        console.log("get方法被访问--------", objProxy === receiver)
-        return Reflect.get(target, key, receiver)
+        console.log("get方法被访问--------", objProxy === receiver);
+        return Reflect.get(target, key, receiver);
+        // return target[key]
     },
     set: function(target, key, newValue, receiver) {
-        console.log("set方法被访问--------")
+        console.log("set方法被访问--------", objProxy === receiver);
         // 传入receiver参数，通过Reflect修改了原对象obj 中getter、setter函数调用时的this指向。
-        Reflect.set(target, key, newValue, receiver)
+        Reflect.set(target, key, newValue, receiver);
+        // target[key] = newValue;
     }
 })
 
-objProxy.name = "zzh"
-console.log(objProxy.name, obj.name)
+console.log('************************');
+objProxy.name = 'zzh';
+console.log(objProxy.name, obj.name);
+
+console.log('************************');
+objProxy.name = 'luke';
+console.log(objProxy.name, obj.name);
