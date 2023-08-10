ff707536d35b69db67b72186427a60361decc154
worldluoji <qq1132416@outlook.com>
2023-08-10 18:56:46 +0800

    refactor: update Promise.race demo

--- js/async/c. promise/4.4 Promise.race.js
+++ js/async/c. promise/4.4 Promise.race.js
@@ -1,15 +1,22 @@
-const promise1 = new Promise((resolve, reject) => {
-    setTimeout((r => {
-        console.log(1);
-        resolve('one');
-    }), 500);
-});
-  
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, 'two');
-});
+/** leetcode 2637
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+ var timeLimit = function(fn, t) {
+    let timeOutPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('Time Limit Exceeded');
+        }, t);
+    });
+	return async function(...args) {
+        // the faster promise will return
+        return Promise.race([fn(...args), timeOutPromise]);
+    }
+};
 
-Promise.race([promise1, promise2]).then((value) => {
-    console.log(value);
-    // Both resolve, but promise2 is faster
-});
+
+const limited = timeLimit((t) => new Promise((resolve) => setTimeout(() => resolve(123), t)), 100);
+limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ 
+limited(99).then(console.log).catch(console.log); 
