fb8a392714da84dd38425b7eb2f648e8a8c1c9bf
worldluoji <qq1132416@outlook.com>
2023-08-11 14:01:31 +0800

    feat: add step-workflow demo

--- js/basics/design/testsingletone.js
+++
@@ -1,7 +0,0 @@
-var CardConfiguration = require('./singletone.js')
-
-var card1 = new CardConfiguration();
-var card2 = new CardConfiguration();
-
-console.log(card1 === card2)      // true
-console.log(card1.name, card1.basePath)
