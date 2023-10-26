95d285cb03c526dd3340eb07532695d54786833c
worldluoji <qq1132416@outlook.com>
2023-10-26 19:50:26 +0800

    feat: add bun express demo

---
+++ js/bun/express/server.ts
@@ -0,0 +1,13 @@
+// bun add express
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
