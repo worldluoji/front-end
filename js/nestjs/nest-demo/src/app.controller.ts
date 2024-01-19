883787e3f76ead3dbb96d36ffa7560e186b81205
worldluoji <qq1132416@outlook.com>
2024-01-19 10:48:47 +0800

    feat: @Post and HttpCode

--- js/nestjs/nest-demo/src/app.controller.ts
+++ js/nestjs/nest-demo/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Req, Query } from '@nestjs/common';
+import { Controller, Get, Post, Req, Query, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 
+// reference: https://docs.nestjs.com/controllers
 @Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -23,5 +24,27 @@ export class AppController {
   @Get('findByType2')
   findByType2(@Query('type') type: string): string {
     return 'This action returns cats with type: ' + type;
+  }
+
+  /* It's that simple. Nest provides decorators for all of the standard HTTP methods: @Get(), @Post(), @Put(), @Delete(), @Patch(), @Options(), and @Head(). 
+  In addition, @All() defines an endpoint that handles all of them */
+  @Post('/create')
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  // The response status code is always 200 by default, except for POST requests which are 201. We can easily change this behavior by adding the @HttpCode(...) decorator at a handler level.
+  @Post('/create2')
+  @HttpCode(204)
+  create2() {
+    return 'This action adds a new cat with httpcode 204';
+  }
+
+  /* The 'ab*cd' route path will match abcd, ab_cd, abecd, and so on. The characters ?, +, *, 
+  and () may be used in a route path, and are subsets of their regular expression counterparts. 
+  The hyphen ( -) and the dot (.) are interpreted literally by string-based paths.*/
+  @Get('ab*cd')
+  findAll() {
+    return 'This route uses a wildcard';
   }
 }
