cd15a036cb6cf4b51260cbf677f23461f829d6a2
worldluoji <qq1132416@outlook.com>
2024-02-08 16:58:26 +0800

    docs: add explanations for this demo

--- micro/webcomponent/stencil-component-demo/src/components/my-component/my-component.tsx
+++ micro/webcomponent/stencil-component-demo/src/components/my-component/my-component.tsx
@@ -1,14 +1,23 @@
 import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
+
+/**
+ * tag: Set the element's name to 'my-component'
+ * styleUrl: Apply the stylesheet 'my-component.css' to the component
+ * shadow: Enable native Shadow DOM functionality for this component
+ */
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
+
   /**
    * The first name
+   * @Prop() tells Stencil that the property is public to the component, 
+   * and allows Stencil to rerender when any of these public properties change
    */
   @Prop() first: string;
 
@@ -26,6 +35,7 @@ export class MyComponent {
     return format(this.first, this.middle, this.last);
   }
 
+  // In order for the component to render something to the screen, we must declare a render() function that returns JSX.
   render() {
     return <div>Hello, World! I'm {this.getText()}</div>;
   }
