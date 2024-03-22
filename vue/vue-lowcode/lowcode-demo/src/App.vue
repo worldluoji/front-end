e526b676f9b00d416c48351c419edf8aec83fa25
worldluoji <qq1132416@outlook.com>
2024-03-22 16:21:52 +0800

    feat: create project with vue3 and shoelance

---
+++ vue/vue-lowcode/lowcode-demo/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <sl-button size="small" variant="primary">Click me</sl-button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
