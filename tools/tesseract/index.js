9b482524f08d713471a43848364917ce418c62a7
worldluoji <qq1132416@outlook.com>
2024-07-15 16:36:16 +0800

    feat: tesseract demo

--- tools/tesseract/index.js
+++ tools/tesseract/index.js
@@ -1,1 +1,9 @@
 import Tesseract from "tesseract.js";
+Tesseract.recognize(
+    './image_with_words.awebp',
+    'eng' // 识别英文
+).then(({data: text}) => {
+    console.log(text);
+}).catch(e => {
+    console.error(e);
+})
