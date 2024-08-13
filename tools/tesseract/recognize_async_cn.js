b45bf89056503cc16975870a0f0d9ac25de10546
worldluoji <qq1132416@outlook.com>
2024-08-13 16:53:45 +0800

    fix: async demo

--- tools/tesseract/recognize_async_cn.js
+++ tools/tesseract/recognize_async_cn.js
@@ -1,11 +1,12 @@
 import Tesseract from "tesseract.js";
 
+// https://github.com/naptha/tesseract.js/blob/master/docs/workers_vs_schedulers.md
 // 创建一个 Tesseract 工作线程
-Tesseract.createWorker('eng').then(async worker => {
+Tesseract.createWorker('chi_sim').then(async worker => {
   try {
     
     // 指定图片文件路径
-    const imagePath = 'image_with_words.awebp';
+    const imagePath = 'image_cn.png';
     
     // 开始 OCR 识别
     const result = await worker.recognize(imagePath);
