9b4524f6c1313323d3e10798319e1a706fd25971
worldluoji <qq1132416@outlook.com>
2024-08-23 14:39:44 +0800

    docs: delete nestjs demo

--- js/nestjs/nest-demo/src/decorator/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { Reflector } from '@nestjs/core';
-
-// The Roles decorator here is a function that takes a single argument of type string[].
-export const Roles = Reflector.createDecorator<string[]>();
