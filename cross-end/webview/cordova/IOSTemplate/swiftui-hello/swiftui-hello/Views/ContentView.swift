5f9cd4a785a2f7a5cf631e47ea528f6df0cab6ee
worldluoji <qq1132416@outlook.com>
2024-09-23 17:23:33 +0800

    feat: add EnvokeUIKitSheetView demo

--- cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/ContentView.swift
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/ContentView.swift
@@ -10,60 +10,10 @@ import SwiftData
 import KeychainSwift
 
 struct ContentView: View {
-    @State private var showUIKitView = false
-
     var body: some View {
-        NavigationView {
-            VStack {
-                // 字体颜色
-                VStack {
-                    // 默认就是居中的
-                    Text("This is a SwiftUI View")
-                        .font(.title)
-                    .foregroundColor(.blue)
-                    
-                    HStack {
-                        Text("Using HStack in horizental dir")
-                        // A spacer expands to make its containing view use all of the space of its parent view, instead of having its size defined only by its contents.
-                        Spacer()
-                        Text("OK")
-                    }
-                }
-                .padding() // 两侧留出一点空隙
-                
-                Divider() // 分割线
-                
-                Button(action: {
-                    self.showUIKitView = true
-                }) {
-                    Text("Go to UIKit View")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-                .sheet(isPresented: $showUIKitView, content: {
-                    MyUIKitViewControllerWrapper()
-                })
-                
-                Spacer()
-            }
-            .navigationTitle("Mixed UI")
-        }
+        EvokeUIKitSheetView()
     }
 }
-
-// 使用 UIViewControllerRepresentable 包装 UIKit 视图控制器
-struct MyUIKitViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> ViewController {
-        return ViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
-        // 更新视图控制器的状态（如果需要）
-    }
-}
-
 
 #Preview {
     ContentView()
---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/EvokeUIKitSheetView.swift
@@ -0,0 +1,60 @@
+//
+//  Views/EvokeUIKitSheetView.swift
+//  swiftui-hello
+//
+//  Created by 罗家懿 on 2024/9/23.
+//
+
+import SwiftUI
+
+struct EvokeUIKitSheetView: View {
+    @State private var showUIKitView = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // 字体颜色
+                VStack {
+                    // 默认就是居中的
+                    Text("This is a SwiftUI View")
+                        .font(.title)
+                    .foregroundColor(.blue)
+                }
+                .padding() // 两侧留出一点空隙
+                
+                Divider() // 分割线
+                
+                Button(action: {
+                    self.showUIKitView = true
+                }) {
+                    Text("Go to UIKit View")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .sheet(isPresented: $showUIKitView, content: {
+                    MyUIKitViewControllerWrapper()
+                })
+                
+                Spacer()
+            }
+            .navigationTitle("Mixed UI")
+        }
+    }
+}
+
+// 使用 UIViewControllerRepresentable 包装 UIKit 视图控制器
+struct MyUIKitViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        // 更新视图控制器的状态（如果需要）
+    }
+}
+
+#Preview {
+    EvokeUIKitSheetView()
+}
