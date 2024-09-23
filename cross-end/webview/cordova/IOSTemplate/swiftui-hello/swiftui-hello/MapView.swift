edad1706ff412951695fc82310291fe017190b4c
worldluoji <qq1132416@outlook.com>
2024-09-23 13:54:18 +0800

    feat: add Map demo

---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  swiftui-hello
+//
+//  Created by 罗家懿 on 2024/9/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
