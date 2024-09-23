af02d7dcbf13473f2e309fafb2d388845c0a35a4
worldluoji <qq1132416@outlook.com>
2024-09-23 12:03:05 +0800

    feat: add CircleImage demo

---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/CircleImageSwift.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImageSwift.swift
+//  swiftui-hello
+//
+//  Created by 罗家懿 on 2024/9/23.
+//
+
+import SwiftUI
+
+struct CircleImageSwift: View {
+    var body: some View {
+        // shanchuan.jpg已经import到了Assets中
+        Image("shanchuan")
+            .frame(width: 350)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/) // Add a call to clipShape(Circle()) to apply the circular clipping shape to the image.
+            .overlay {
+                // Create another circle with a gray stroke, and then add it as an overlay to give the image a border.
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7) // add a shadow with a 7 point radius.
+            .padding()
+    }
+}
+
+#Preview {
+    CircleImageSwift()
+}
