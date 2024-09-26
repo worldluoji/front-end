f66ef9fb25b6d6ea0a86b383dd95bccdf0e55863
worldluoji <qq1132416@outlook.com>
2024-09-26 20:07:26 +0800

    feat: add Bandage demo

---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/Bandage/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  swiftui-hello
+//
+//  Created by Luke-Surface-mac on 2024/9/26.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/Landmarks/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  swiftui-hello
+//
+//  Created by Luke-Surface-mac on 2024/9/26.
+//
+
+import Foundation
