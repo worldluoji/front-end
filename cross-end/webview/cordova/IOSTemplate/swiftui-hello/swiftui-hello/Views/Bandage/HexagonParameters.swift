0caab022fd1e8837a589c9210e274d9b4b70cfce
worldluoji <qq1132416@outlook.com>
2024-09-26 20:07:46 +0800

    feat: add Bandage dmeo

---
+++ cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/Bandage/HexagonParameters.swift
@@ -0,0 +1,58 @@
+//
+//  HexagonParameters.swift
+//  swiftui-hello
+//
+//  Created by Luke-Surface-mac on 2024/9/26.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    // Define a Segment structure to hold the three points that represent one side of the hexagon; import CoreGraphics so you can use CGPoint.
+    
+    // Each side starts where the previous ends, moves in a straight line to the first point, and then moves over a Bézier curve at the corner to the second point. The third point controls the shape of the curve.
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+    
+    
+    // Add an adjustment value that lets you tune the shape of the hexagon.
+    static let adjustment: CGFloat = 0.085
+    
+    // The values are stored as a fraction of a unit square having its origin in the upper left, with positive x to the right and positive y down. Later, you’ll use these fractions to find the actual points of a hexagon with a given size.
+    static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+}
--- cross-end/webview/cordova/IOSTemplate/swiftui-hello/swiftui-hello/Views/Landmarks/HexagonParameters.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HexagonParameters.swift
-//  swiftui-hello
-//
-//  Created by Luke-Surface-mac on 2024/9/26.
-//
-
-import Foundation
