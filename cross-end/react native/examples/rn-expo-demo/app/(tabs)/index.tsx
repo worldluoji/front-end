82815617b0a759ff3828bd7fd9016d2ca67a2100
worldluoji <qq1132416@outlook.com>
2024-11-06 17:27:12 +0800

    feat: add components

--- cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
+++ cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const styles = StyleSheet.create({
   container: {
@@ -8,23 +9,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: '#25292e',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    /*
+     flex: 1 / 3：这里的 flex-grow 1 / 3 实际上会被计算为 0.3333...。这意味着 footerContainer 将占据其父容器高度的约1/3。
+     在React Native中，View 组件的默认 flexDirection 是 column
+    */
+    flex: 1 / 3,
+    alignItems: 'center',
   }
 })
+
+const PlaceHolderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={ styles.container}>
-      <Text style={ styles.text }>Home Screen</Text>
-      <Link href="/about" style={ styles.button }>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={ PlaceHolderImage } />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
---
+++ cross-end/react native/examples/rn-expo-demo/components/Button.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+type Props = {
+  label: string;
+};
+
+export default function Button({ label }: Props) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
