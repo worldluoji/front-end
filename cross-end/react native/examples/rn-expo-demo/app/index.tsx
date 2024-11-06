3e51963b131442e968d5af56b5d1ea1a41364f91
worldluoji <qq1132416@outlook.com>
2024-11-06 14:02:56 +0800

    refactor: update home page

--- cross-end/react native/examples/rn-expo-demo/app/index.tsx
+++ cross-end/react native/examples/rn-expo-demo/app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#25292e',
+  },
+  text: {
+    color: '#fff',
+  }
+})
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={ styles.container}>
+      <Text style={ styles.text }>Home Screen</Text>
     </View>
   );
 }
