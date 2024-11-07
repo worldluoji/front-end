d6409d50533b12dc5e9505a3a1b7e0f3bb889ad3
worldluoji <qq1132416@outlook.com>
2024-11-07 11:02:52 +0800

    feat: add CircleButton、IconButton and EmojiPicker

--- cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
+++ cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import { View, StyleSheet } from "react-native";
+import { useState } from 'react';
+
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 const styles = StyleSheet.create({
   container: {
@@ -21,6 +25,14 @@ const styles = StyleSheet.create({
     */
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   }
 })
 
@@ -28,6 +40,8 @@ const PlaceHolderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isMoalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -37,9 +51,26 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  }
+
+  const onSaveImageAsync = async () => {
+    
   };
 
   return (
@@ -47,10 +78,23 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" onPress={ pickImageAsync }/>
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button label="Choose a photo" theme="primary" onPress={ pickImageAsync }/>
+          <Button label="Use this photo" />
+        </View>
+      )}
+      <EmojiPicker isVisiable={isMoalVisible} onClose={onModalClose}>
+
+      </EmojiPicker>
     </View>
   );
 }
