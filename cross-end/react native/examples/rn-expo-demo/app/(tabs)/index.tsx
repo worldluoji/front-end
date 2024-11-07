88446167c50c48fbba4166c98c139895860c12d7
worldluoji <qq1132416@outlook.com>
2024-11-07 18:47:42 +0800

    feat: add web screenshot

--- cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
+++ cross-end/react native/examples/rn-expo-demo/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import { useState, useRef } from 'react';
 import { type ImageSource } from 'expo-image';
 import * as MediaLibrary from 'expo-media-library';
@@ -12,6 +12,7 @@ import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
+import domtoimage from 'dom-to-image';
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -86,17 +87,33 @@ export default function Index() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Saved!');
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440
+        })
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
---
+++ tools/screenshot/screenshot.md
@@ -0,0 +1,4 @@
+# screenshot
+To capture a screenshot on the web and save it as an image, we'll use a third-party library called dom-to-image. It takes a screenshot of any DOM node and turns it into a vector (SVG) or raster (PNG or JPEG) image.
+
+https://github.com/tsayen/dom-to-image#readme
