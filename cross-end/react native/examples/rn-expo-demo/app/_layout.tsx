e5594d7fbd7166a7704d8d17947a208091e263d5
worldluoji <qq1132416@outlook.com>
2024-11-11 09:30:39 +0800

    feat: set statusbar style

--- cross-end/react native/examples/rn-expo-demo/app/_layout.tsx
+++ cross-end/react native/examples/rn-expo-demo/app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle('light');
+    }, 0);
+  }, []);
+  
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
