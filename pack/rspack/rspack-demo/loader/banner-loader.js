7285714601d5694dc83643d36c86b04a50cb8f87
worldluoji <qq1132416@outlook.com>
2024-11-18 16:59:00 +0800

    refactor: delete rspack

--- pack/rspack/rspack-demo/loader/banner-loader.js
+++
@@ -1,8 +0,0 @@
-const BANNER = `/**
- * Copyright (c) 2023-present Luke Luo, Inc.
- * https://github.com/worldluoji/front-end
- */`;
-
-module.exports = function (content) {
-  return `${BANNER}\n${content}`;
-};
