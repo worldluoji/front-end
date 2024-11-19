9861b6f5aa1ec6da783d4c4d094e8c5e0a9edb4b
worldluoji <qq1132416@outlook.com>
2024-11-19 15:53:36 +0800

    refactor: 优化createRedux类型定义

--- react/vite-react-demo/src/components/redux/CreateReducerDemo.tsx
+++ react/vite-react-demo/src/components/redux/CreateReducerDemo.tsx
@@ -17,7 +17,18 @@ interface StateType {
   count: number
 }
 
-const useThunkReducer = createReducer(thunk) as (reducer: (state: StateType, action: ActionType) => StateType, initialState: StateType, initializer?: ((value: StateType) => StateType) | undefined) => [StateType, Dispatch<ActionType>];
+// const useThunkReducer = createReducer(thunk) as (reducer: (state: StateType, action: ActionType) => StateType, initialState: StateType, initializer?: ((value: StateType) => StateType) | undefined) => [StateType, Dispatch<ActionType>];
+
+/* 使用泛型指定状态类型和动作类型，
+   useThunkReducer 被定义为一个泛型函数, 接受两个类型参数 S 和 A，分别代表状态类型和动作类型。
+   A extends { type: any }：代表动作类型（Action Type），并且动作类型必须有一个 type 属性。
+   这里的 as 断言是一个类型断言，用于告诉 TypeScript 编译器我们希望 createReducer(thunk) 返回的函数具有特定的类型。
+*/
+const useThunkReducer = createReducer(thunk) as <S, A extends { type: any }>(
+  reducer: (state: S, action: A) => S,
+  initialState: S,
+  initializer?: (value: S) => S
+) => [S, Dispatch<A>];
 
 function reducer(state: StateType, action: ActionType) {
   switch (action.type) {
@@ -34,7 +45,7 @@ function reducer(state: StateType, action: ActionType) {
 
 const CreateReducerDemo = ({ initialCount = 1 }) => {
   
-  const [state, dispatch] = useThunkReducer(reducer, { count: initialCount });
+  const [state, dispatch] = useThunkReducer<StateType, ActionType>(reducer, { count: initialCount });
 
   // Action creator to increment count, wait a second and then reset
   const addAndReset = useCallback(() => {
