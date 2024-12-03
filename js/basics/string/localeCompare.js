c0b1919f5a672fdacef8f4fb36dd4ee4577cb285
worldluoji <qq1132416@outlook.com>
2024-12-03 15:35:27 +0800

    refactor: update

--- js/basics/string/localeCompare.js
+++ js/basics/string/localeCompare.js
@@ -1,5 +1,7 @@
 
 /* 在 JavaScript 中，字符串的字典序比较可以使用全局函数 localeCompare 实现。该函数返回一个整数，表示两个字符串在字典序上的比较结果。
-如果第一个字符串小于第二个字符串，则返回负数；如果两个字符串相等，则返回 0；如果第一个字符串大于第二个字符串，则返回正数。*/
+如果第一个字符串按字典序在第二个字符串之后，返回正数；如果两个字符串相等，则返回 0；否则返回负数。*/
 
-console.log('bcd'.localeCompare('abcde'))
+console.log('bcd'.localeCompare('abcde'));
+
+console.log('3'.includes(3)); // true
