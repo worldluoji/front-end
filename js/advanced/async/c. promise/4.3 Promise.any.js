564791639489d14df86265fe43f85f4eba7fa415
worldluoji <qq1132416@outlook.com>
2024-12-09 11:07:20 +0800

    fix: update promise.any demo

--- js/advanced/async/c. promise/4.3 Promise.any.js
+++ js/advanced/async/c. promise/4.3 Promise.any.js
@@ -7,6 +7,6 @@ const promise3 = new Promise((resolve) => setTimeout(resolve, 500, 'quick'));
 
 const promises = [promise1, promise2, promise3];
 
-Promise.any(promises).then((value) => console.log(value));
+Promise.any(promises).then((value) => console.log(`${value} is faster`));
 
 // 虽然一个Promise完成就返回，但是其它的promise会继续执行
