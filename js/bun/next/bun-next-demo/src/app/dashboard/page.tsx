a0ac55eca10605237be1987c3512eabac9c0c3da
worldluoji <qq1132416@outlook.com>
2024-12-09 16:23:05 +0800

    feat: add title

--- js/bun/next/bun-next-demo/src/app/dashboard/page.tsx
+++ js/bun/next/bun-next-demo/src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+
+import type { Metadata } from 'next'
+
+// You can modify the <head> HTML elements such as title and meta using the Metadata APIs.
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+ 
 export default function Page() {
     return <h1>Hello, Dashboard Page!</h1>
 }
