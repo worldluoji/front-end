492fdc6c5565f3e141d3c1dd16c1df626ad2c7d8
worldluoji <qq1132416@outlook.com>
2024-12-10 09:29:26 +0800

    feat: useRouter and redirect

--- js/bun/next/bun-next-demo/src/app/page.tsx
+++ js/bun/next/bun-next-demo/src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client"
 import Image from "next/image";
+// client component can use useRouter from next/navigation
+import { useRouter } from 'next/navigation';
+
+// server component can use redirect from next/navigation
+// import { redirect } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -95,6 +102,9 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <button type="button" onClick={() => router.push('/dashboard')}>
+          Dashboard
+        </button>
       </footer>
     </div>
   );
