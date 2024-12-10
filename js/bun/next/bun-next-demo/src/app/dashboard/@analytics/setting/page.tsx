bdc80609eecd7261c80d44568557ccef49b3c6cd
worldluoji <qq1132416@outlook.com>
2024-12-10 20:32:36 +0800

    feat: add default page demo

---
+++ js/bun/next/bun-next-demo/src/app/dashboard/@analytics/setting/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AnalyticsSettingPage() {
+    return (
+        <div>
+            <p>analytics setting page</p>
+        </div>
+    )
+}
---
+++ js/bun/next/bun-next-demo/src/app/dashboard/default.tsx
@@ -0,0 +1,8 @@
+
+export default function Default() {
+  return (
+    <div className="flex items-center justify-center h-40 bg-blue-500 text-white">
+      <h1>This is the dashboard default page</h1>
+    </div>
+  )
+}
