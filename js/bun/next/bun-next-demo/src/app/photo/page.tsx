0ab109158f6f83ac1d8f65ac3d375cdd998482e0
worldluoji <qq1132416@outlook.com>
2024-12-11 17:17:31 +0800

    feat: add photo page

---
+++ js/bun/next/bun-next-demo/src/app/photo/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamPage() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p>photo page</p>
+        </div>
+    )
+}
