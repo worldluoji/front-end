27bad23d969292f52d567f36e0d6a31817da5a93
worldluoji <qq1132416@outlook.com>
2024-12-13 15:49:13 +0800

    fix: remove unnecessary import

--- react/vite-react-demo/src/hooks/useKeyPress.ts
+++ react/vite-react-demo/src/hooks/useKeyPress.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 const useKeyPress = (domNode = document.body) => {
     const [key, setKey] = useState<string>()
