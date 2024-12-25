4cfdb389b67079dac4c2bb307a6ebc12359d0552
worldluoji <qq1132416@outlook.com>
2024-12-25 11:19:56 +0800

    refactor: add defer to TeleportModal

--- vue/vite-vue3-demo/src/components/builtplugin/TeleportModal.vue
+++ vue/vite-vue3-demo/src/components/builtplugin/TeleportModal.vue
@@ -11,8 +11,13 @@ const open = ref(false);
 
 <template>
   <button @click="open = true">Open Modal</button>
-  <!-- 为 <Teleport> 指定的目标 to 期望接收一个 CSS 选择器字符串或者一个真实的 DOM 节点。这里我们其实就是让 Vue 去“传送这部分模板片段到 body 标签下”。 -->
-  <Teleport to="body">
+  <!-- 
+    为 <Teleport> 指定的目标 to 期望接收一个 CSS 选择器字符串或者一个真实的 DOM 节点。这里我们其实就是让 Vue 去“传送这部分模板片段到 body 标签下”。 
+    
+    内置 <Teleport> 组件的一个已知约束是在 teleport 组件挂载时其目标元素必须存在。这可以防止用户在 teleport 后将内容传送到 Vue 渲染的其他元素。
+    在 Vue 3.5 中，我们为 <Teleport> 引入了 defer prop，它会在当前渲染周期之后挂载它。
+  -->
+  <Teleport defer to="body">
     <div v-if="open" class="modal">
       <p>Hello from the modal!</p>
       <button @click="open = false">Close</button>
