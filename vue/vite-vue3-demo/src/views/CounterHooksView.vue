75db7806dffce22fce8e2d9df99f6342705b5a7e
worldluoji <qq1132416@outlook.com>
2025-01-02 09:41:39 +0800

    fix: 修复ts报错

--- vue/vite-vue3-demo/src/views/CounterHooksView.vue
+++ vue/vite-vue3-demo/src/views/CounterHooksView.vue
@@ -1,9 +1,9 @@
 <template>
-    <div @click="increment">{{ count }}</div>
-  </template>
+  <div @click="increment">{{ count }}</div>
+</template>
   
-  <script setup>
-  import useCounter from '@/hooks/useCounter.ts';
-  const { count, increment } = useCounter(10);
-  </script>
+<script setup lang="ts">
+import useCounter from '../hooks/useCounter.ts';
+const { count, increment } = useCounter(10);
+</script>
   
