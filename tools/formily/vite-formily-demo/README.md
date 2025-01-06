221d8092553e59dcb017bdb693b8afd98ec753a6
worldluoji <qq1132416@outlook.com>
2025-01-06 14:03:35 +0800

    feat: add formily demo

---
+++ tools/formily/vite-formily-demo/README.md
@@ -0,0 +1,9 @@
+# Formily react demo
+注意 不能是ant5版本，否则找不到index.less文件。
+https://github.com/ant-design/pro-components/issues/4880
+
+## 快速开始
+```shell
+pnpm install
+pnpm dev
+```
---
+++ tools/formily/vite-formily-demo/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^~/, 
+        replacement: '',
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+})
