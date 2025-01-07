a09395c26338a619b914802998e5dba3eba65a74
worldluoji <qq1132416@outlook.com>
2025-01-07 15:18:10 +0800

    fix: add useAttrs to get confuration

--- tools/formily/vite-epicdesigner-demo/src/designer-extensions/test/index.vue
+++ tools/formily/vite-epicdesigner-demo/src/designer-extensions/test/index.vue
@@ -2,12 +2,18 @@
     <div class="container">
         <p>我是一个扩展模块</p>
         <p>组件id: {{ checkedNode?.id }}</p>
+        <p>组件placement: {{ attrs?.placement }}</p>
     </div>
 </template>
 <script lang="ts" setup>
-import { inject, computed } from 'vue'
+import { inject, computed, useAttrs } from 'vue'
 import { type Designer } from 'epic-designer'
 const designer = inject('designer') as Designer
+
+// useAttrs() 返回的是一个响应式的引用（ref），这意味着当父组件传递给子组件的非 prop 属性发生变化时，attrs 也会相应地更新
+// 配置文件index.ts里的配置，会自动绑定到attrs上（也就是组件的props属性）
+let attrs = useAttrs()
+console.log(1, attrs)
 
 // 可以从designer.state里面取到选中的组件的配置的值
 console.log(111, designer);
