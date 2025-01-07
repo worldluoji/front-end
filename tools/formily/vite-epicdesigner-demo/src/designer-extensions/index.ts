7314d6b21a0af47bed0b1a32a0abb36612e25904
worldluoji <qq1132416@outlook.com>
2025-01-07 10:53:12 +0800

    feat: 添加公共函数

--- tools/formily/vite-epicdesigner-demo/src/designer-extensions/index.ts
+++ tools/formily/vite-epicdesigner-demo/src/designer-extensions/index.ts
@@ -1,9 +1,13 @@
 import { pluginManager } from "epic-designer"
 import Test from "./test"
+import { setupPublicMethod } from './script/index'
 
 // 安装扩展
 export function setupDesignerExtensions(): void {
 
      // 注册组件
      pluginManager.registerComponent(Test);
+
+     // 注册公共方法
+     setupPublicMethod();
 }
