26ba2e06807114ffc2a5f002b952e14f00a7e104
worldluoji <qq1132416@outlook.com>
2025-01-07 14:01:01 +0800

    fix: 获取选中组件的配置

--- tools/formily/vite-epicdesigner-demo/src/designer-extensions/test/index.vue
+++ tools/formily/vite-epicdesigner-demo/src/designer-extensions/test/index.vue
@@ -2,13 +2,15 @@
     <div class="container">
         <p>我是一个扩展模块</p>
         <p>组件id: {{ checkedNode?.id }}</p>
+        <p>组件placement: {{ checkedNode?.componentProps.placement }}</p>
     </div>
 </template>
 <script lang="ts" setup>
 import { inject, computed } from 'vue'
 import { type Designer } from 'epic-designer'
 const designer = inject('designer') as Designer
-
+// 可以从designer.state里面取到选中的组件的配置的值
+console.log(111, designer);
 const checkedNode = computed(() => {
     return designer.state.checkedNode
 })
