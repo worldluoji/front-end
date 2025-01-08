b119bde03662752826278154005ed9ce611dd7f3
worldluoji <qq1132416@outlook.com>
2025-01-08 15:27:50 +0800

    fix: 使用import.meta.glob批量加载组件

--- tools/formily/vite-faster-design/packages/extensions/index.ts
+++ tools/formily/vite-faster-design/packages/extensions/index.ts
@@ -1,12 +1,19 @@
 import { pluginManager } from "epic-designer"
 import { setupPublicMethod } from './script/index.js'
 
-import QRCode from './components/QRCode'
+// 定义组件模块的类型
+interface ComponentModule {
+     default: any;
+}
+
+const components = import.meta.glob<ComponentModule>('./components/**/*.ts', { eager: true })
 
 // 安装扩展
 export function setupDesignerExtensions(): void {
      // 注册组件
-     pluginManager.registerComponent(QRCode)
+     Object.entries(components).forEach(([_, module]) => {
+          pluginManager.registerComponent(module.default)
+     });
 
      // 注册公共方法
      setupPublicMethod()
