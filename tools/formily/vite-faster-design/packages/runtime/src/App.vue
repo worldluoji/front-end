7d5eb6c3513c95e3dbf21cfc1c4b8f4883b59ec1
worldluoji <qq1132416@outlook.com>
2025-01-08 10:45:02 +0800

    feat: add runtime

---
+++ tools/formily/vite-faster-design/packages/runtime/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <EBuilder :pageSchema="pageSchema" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { EBuilder } from "epic-designer"
+
+const pageSchema = {
+  schemas: [
+    {
+      type: "page",
+      id: "root",
+      children: [
+        {
+          label: "输入框",
+          type: "input",
+          field: "input",
+          icon: "epic-icon-write",
+          input: true,
+          componentProps: {
+            defaultValue: "",
+            placeholder: "请输入",
+            size: "default",
+            type: "text",
+          },
+          id: "gbm1xhrrj5s00",
+        },
+      ],
+      componentProps: {
+        style: {
+          padding: "16px",
+        },
+      },
+    },
+  ],
+};
+</script>
---
+++ tools/formily/vite-faster-design/packages/runtime/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 引入epic-designer样式
+import "epic-designer/dist/style.css"
+
+// 引入Element plus样式
+import "element-plus/dist/index.css"
+
+import { setupElementPlus } from "epic-designer/dist/ui/elementPlus"
+
+// 注册Element UI
+setupElementPlus()
+createApp(App).mount('#app')
