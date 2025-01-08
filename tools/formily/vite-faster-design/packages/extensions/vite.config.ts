4a9fa0868aa2ad0874d72b0643491ca4eaaee2fd
worldluoji <qq1132416@outlook.com>
2025-01-08 17:24:05 +0800

    feat: add postcss-nested

--- tools/formily/vite-faster-design/packages/extensions/vite.config.ts
+++ tools/formily/vite-faster-design/packages/extensions/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcssNested from 'postcss-nested'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  css: {
+    postcss: {
+      plugins: [postcssNested()]
+    }
+  }
 })
