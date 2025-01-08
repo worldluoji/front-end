e6b5ac4457d5b7c6d3bad51cb845a81fb05b0371
worldluoji <qq1132416@outlook.com>
2025-01-08 17:07:13 +0800

    feat: runtime增加打包降级

--- tools/formily/vite-faster-design/packages/runtime/vite.config.ts
+++ tools/formily/vite-faster-design/packages/runtime/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['last 2 versions', '> 0.2%', 'not dead'],
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['last 2 versions', '> 0.2%', 'not dead']
+        })
+      ]
+    }
+  },
 })
