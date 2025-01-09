794c85fc14407b12755f6f4c7e2353e6ee63f292
worldluoji <qq1132416@outlook.com>
2025-01-09 08:40:07 +0800

    feat: add postcssNested for runtime

--- tools/formily/vite-faster-design/packages/runtime/vite.config.ts
+++ tools/formily/vite-faster-design/packages/runtime/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
 import autoprefixer from 'autoprefixer'
+import postcssNested from 'postcss-nested'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,7 +18,8 @@ export default defineConfig({
         autoprefixer({
           // 指定目标浏览器
           overrideBrowserslist: ['last 2 versions', '> 0.2%', 'not dead']
-        })
+        }),
+        postcssNested()
       ]
     }
   },
