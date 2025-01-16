67297110bfe7076b43ed424a3e122196e99cec38
worldluoji <qq1132416@outlook.com>
2025-01-16 16:18:30 +0800

    format: 格式调整

--- vue/vite-vue3-demo/vite.config.ts
+++ vue/vite-vue3-demo/vite.config.ts
@@ -6,7 +6,10 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx()
+  ],
   resolve: {
     alias: {
       '@': path.join(__dirname, '/src')
