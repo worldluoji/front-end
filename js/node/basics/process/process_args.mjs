a30d9d2f09a5da3648dfb03d12c1298a5a6520a9
worldluoji <qq1132416@outlook.com>
2025-03-14 16:44:11 +0800

    feat: add cwd() and __filename

--- js/node/basics/process/process_args.mjs
+++ js/node/basics/process/process_args.mjs
@@ -1,4 +1,6 @@
 import process from "node:process";
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
 // node process_args.mjs arg1 arg2
 console.log(process.argv);
@@ -9,3 +11,17 @@ console.log(process.argv);
 
 // 所以可以知道，我们能够放心地使用 process.argv.slice(2) 来获取所需要的参数
 console.log(process.argv.slice(2));
+
+
+// 这是指执行 Node.js 进程时所在的目录，而不是当前脚本文件所在的目录。注意与 __dirname (仅commonjs有) 区分。
+console.log('Current Working Directory:', process.cwd());
+
+
+// Get the file URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name
+const __dirname = path.dirname(__filename);
+
+// cd ..   node ./process/process_args.mjs arg1 arg2
+console.log('file dir:', __dirname);
