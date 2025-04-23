36be65870f80e2b4dd03fb5b0cea31797e432b62
worldluoji <qq1132416@outlook.com>
2025-04-23 14:16:36 +0800

    fix: solve the inline js error

--- browser/plugin/dom-selector-tool/docs/instruction.js
+++ browser/plugin/dom-selector-tool/docs/instruction.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     // 默认显示英文内容
     document.getElementById('content-zh').classList.add('hidden');
+    const enButton = document.getElementById('en-button');
+    const cnButton = document.getElementById('cn-button');
+    
+    enButton.addEventListener('click', () => {
+        switchLanguage('en');
+    });
+
+    cnButton.addEventListener('click', () => {
+      switchLanguage('zh');
+    });
 });
 
 function switchLanguage(lang) {
