ee686b9fd1d499120d32ca2004a8d5f3aad84122
worldluoji <qq1132416@outlook.com>
2025-05-12 15:17:46 +0800

    feat: add object.assign demo

---
+++ js/basics/obj/obj-assign.js
@@ -0,0 +1,9 @@
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target);
