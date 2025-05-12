c043dc901fe581f79af4c2ffe0dffe49126da878
worldluoji <qq1132416@outlook.com>
2025-05-12 15:18:54 +0800

    fix: remove annotation

--- js/basics/obj/obj-fromentries.js
+++ js/basics/obj/obj-fromentries.js
@@ -14,5 +14,3 @@ entries2.set('foo', 'bar');
 entries2.set('baz', 42);
 const obj2 = Object.fromEntries(entries2);
 console.log(obj2);
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
