e7eb8abed90c479e580e74adf774295fa16fdc0d
worldluoji <qq1132416@outlook.com>
2025-05-13 11:15:40 +0800

    docs: add annotations

--- js/node/basics/event/event.mjs
+++ js/node/basics/event/event.mjs
@@ -16,5 +16,10 @@ myEmitter.on('event2', function(a, b) {
 // trigger
 myEmitter.emit('event', 'Hello', 'event');
 
+/**
+ *普通函数有自身的 this，其值取决于函数调用方式。
+ *当你通过 myEmitter.emit('event2') 触发事件时，EventEmitter 内部会以类似 handler.call(myEmitter, ...args) 的方式调用监听函数。
+ *因此，this 被动态绑定为 myEmitter 实例。 
+ **/ 
 myEmitter.emit('event2', 'Hello', 'event');
 myEmitter.emit('event2', 'Hello', 'world');
