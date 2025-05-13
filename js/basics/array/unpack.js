b89af74c20cae9e9b0432e2e09bc3ce033c4b296
worldluoji <qq1132416@outlook.com>
2025-05-13 15:17:12 +0800

    feat: update unpack demo

--- js/basics/array/unpack.js
+++ js/basics/array/unpack.js
@@ -6,3 +6,11 @@ b[1][1] = 6;
 console.log(a);
 console.log(b);
 console.log(a[1] === b[1] ? '...是浅拷贝': '...不是浅拷贝');
+
+// Rest properties
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+console.log(x, y, z);
+
+// let { x, ...z, b } = { x: 1, y: 2, a: 3, b: 4 };
+// console.log(x, y, z);
+// VM149:1 Uncaught SyntaxError: Rest element must be last element
