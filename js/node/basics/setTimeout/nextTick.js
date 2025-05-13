60c94622a7675748e8217df5dd4ab4042fb9ab56
worldluoji <qq1132416@outlook.com>
2025-05-13 17:16:03 +0800

    docs: add annotations

--- js/node/basics/setTimeout/nextTick.js
+++ js/node/basics/setTimeout/nextTick.js
@@ -1,7 +1,11 @@
 // one important part is process.nextTick(). Every time the event loop takes a full trip, we call it a tick.
 console.log("Hello => number 1");
 
-// Calling setTimeout(() => {}, 0) will execute the function at the end of next tick, much later than when using nextTick() which prioritizes the call and executes it just before the beginning of the next tick.
+/*
+setTimeout(fn, 0) 和 setImmediate（进入事件循环队列）​​
+​**setTimeout(fn, 0)​：延迟设为 0 的定时器会被 Node.js 强制设置为 1ms，回调进入 ​Timers 阶段**。
+​**setImmediate​：回调进入 ​Check 阶段**，在事件循环的一次完整迭代（tick）结束后执行。
+*/
 setTimeout(() => {
   console.log("The timeout running last => number 4");
 }, 0);
@@ -18,6 +22,7 @@ setTimeout(() => {
     console.log("The timeout running last => number 4.3");
 }, 0);
 
+// nextTick 回调会在当前事件循环阶段（当前操作）结束后 ​立即执行，优先级高于其他异步操作。
 process.nextTick(() => {
   console.log("Running at next tick => number 2");
 });
